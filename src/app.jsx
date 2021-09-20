b19942969ea4557a4d63d930f063984971b4b5af
Ozziekins <nenubariozioma@gmail.com>
2021-09-20 15:03:28 +0300

    errors on build and start handled

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import { hot } from 'react-hot-loader/root'
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>Hello from react 4</h1>
+    </>
+  )
+}
+
+export default hot(App)
